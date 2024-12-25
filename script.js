847dac9c8b7eea5d50eb5ad9aac10956da694062
iiVertex <ammaryahia23@gmail.com>
2024-12-25 22:06:26 +0300

    responsive design adds

---
+++ script.js
@@ -0,0 +1,5 @@
+// Function to toggle the menu
+function toggleMenu() {
+    const menu = document.querySelector('.small-menu');
+    menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
+}
